046b083529ab5edd57455c94ad7bff8ceeb6013f
Vonnegutter1 <50712501+Vonnegutter1@users.noreply.github.com>
2019-07-02 08:33:27 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -11,15 +11,18 @@ SELECT * FROM "accounts" WHERE "account_balance" = 0 AND "transactions_attempted
 UPDATE "accounts" SET "account_balance" = 10 WHERE "user_id" = 5; 
 
 How do you select all users that have attempted 9 or more transactions?
-
-
-How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+SELECT * FROM "accounts" where "transactions_attempted" >= '9';
+How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? 
+NOTE: Research LIMIT
+SELECT "username", "account_balance" from "accounts" order by "account_balance" desc limit 3;
 How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+SELECT "username", "account_balance" from "accounts" order by "account_balance" acs limit 3;
 How do you get all users with account balances that are more than $100?
+SELECT * FROM "accounts" where "account_balance" > 100.00;
 How do you add a new account?
 INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance) VALUES ('danielle', 'minneapolis', 3, 13, 2000.00);
 The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
-
+SELECT * FFROM "accounts" where "transactions
 
 -- Limit the amount of stuff returned 
 SELECT * FROM "songs" LIMIT 10;
